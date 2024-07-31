f73a08214386d74e8f1416d8665669b2020f6f89
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-07-31 15:45:38 -0300

    finished lesson 31

---
+++ MODULE-1/LESSON31-BASIC-APACHE-WEBSERVER-CONFIGURATION-AND-VIRTUAL-HOSTS/with-htaccess-file/htdocs/pages/contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Page</title>
+</head>
+
+<body>
+    <?php
+    include_once 'partials/navbar.php';
+
+    ?>
+    <h1>Home Page</h1>
+    <p>Hello from the Contact Page!</p>
+</body>
+
+</html>
