17f192621742fb07b465fa88fd3d4b34342198fb
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-08-14 18:41:29 -0300

    finished lesson 5

--- MODULE-3/phpunit-first-example/app/Router.php
+++ MODULE-3/phpunit-first-example/app/Router.php
@@ -67,16 +67,11 @@ class Router
         if (is_array($action)) {
             [$class, $method] = $action;
             if (class_exists($class)) {
-
-                $class = new $class;
-
+                $class = new $class();
                 if (method_exists($class, $method)) {
                     return call_user_func_array([$class, $method], []);
                 }
-
             }
-
-
         }
 
         throw new RouteNotFoundException();
