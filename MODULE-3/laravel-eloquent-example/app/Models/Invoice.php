62e8b876c5fb689e56b14ef2b2026a2f0a8d0ca9
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-08-30 17:59:32 -0300

    finished lesson 21

--- MODULE-3/laravel-eloquent-example/app/Models/Invoice.php
+++ MODULE-3/laravel-eloquent-example/app/Models/Invoice.php
@@ -1,38 +1,46 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\InvoiceStatus;
 use Illuminate\Database\Eloquent\Model;
 
 // * Doc blocks should be provided, with your Eloquent Models, to generate better autocomplete for your fellow developers (and yourself):
+
 /**
  * @property int $id
  * @property string $invoice_number
- * @property float $amount 
- * @property InvoiceStatus $status
+ * @property float $amount
+ * @property InvoiceStatus  $status
  * @property Carbon $created_at
  * @property Carbon $due_date
+ *
+ * @property-read Collection $items
  */
 class Invoice extends Model
 {
-    protected $table = 'invoices'; // used if you want to define the corresponding table name explicitly
+    // * This is used if you want the values returned by these models to be cast/transformed to specific types.
+    protected $casts = [
+        'created_at' => 'datetime',
+        'due_date' => 'datetime',
+        'status' => InvoiceStatus::class
+    ];
 
-    protected $primaryKey = 'invoice_uid'; // we define a 'primaryKey' property if we want to use a field that isn't 'ID' as the primary key of this specific table. 
+    protected $table = 'invoices';  // used if you want to define the corresponding table name explicitly
 
-    protected $incrementing = false; // we define this property to tell Laravel that we don't want to use the auto-incrementing feature of the database
+    // protected $primaryKey = 'invoice_uid';  // we define a 'primaryKey' property if we want to use a field that isn't 'ID' as the primary key of this specific table.
 
-    protected $keyType = 'string'; // we define this property to tell Laravel that we want to use a string as the type of the primary key of this specific table.
+    // public $incrementing = false;  // we define this property to tell Laravel that we don't want to use the auto-incrementing feature of the database, with our keys
 
-    protected $timestamps = false; // we define this property to tell Laravel that we don't want to use the timestamps feature (created_at and updated_at) of the database
+    // protected $keyType = 'string';  // we define this property to tell Laravel that we want to use a string as the type of the primary key of this specific table.
+
+    public $timestamps = false;  // we define this property to tell Laravel that we don't want to use the timestamps feature (created_at and updated_at) of the database
 
     public function __construct() {}
 
-    const CREATED_AT = 'created_date'; // we define a 'created_at' property if we want to use a field that isn't 'created_at' as the column name of the 'created_at' column of this specific table.
+    // const CREATED_AT = 'created_date';  // we define a 'created_at' property if we want to use a field that isn't 'created_at' as the column name of the 'created_at' column of this specific table.
 
-    const UPDATED_AT = null; // we define a 'updated_at' property as null if we don't want to use/don't have a field that isn't 'updated_at' as the column name of the 'updated_at' column of this specific table.
-
+    const UPDATED_AT = null;  // we define a 'updated_at' property as null if we don't want to use/don't have a field that isn't 'updated_at' as the column name of the 'updated_at' column of this specific table.
 
     // * This is what sets the relation between the Invoice and the InvoiceItem
     public function items()
--- MODULE-3/laravel-eloquent-example/app/eloquent_example.php
+++ MODULE-3/laravel-eloquent-example/app/eloquent_example.php
@@ -1,17 +1,15 @@
 <?php declare(strict_types=1);
 
+use App\Enums\InvoiceStatus;
+use App\Models\Invoice;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require __DIR__ . '/../eloquent.php';
 
-
 // \Illuminate\Database\Capsule\Manager::connection(); // This provides us access to the database connection, which is used by the Eloquent ORM. We can use it to call rollback(), commit() and 'transaction()' (which works like transactional, will run a closure, everything inside of it, commit if everything goes right, and rollback if something goes wrong).
 
-
-
-
-\Illuminate\Database\Capsule\Manager::connection()->transaction(function() {
-
+\Illuminate\Database\Capsule\Manager::connection()->transaction(function () {
     $invoice = new \App\Models\Invoice();
 
     $invoice->amount = 45;
@@ -21,9 +19,9 @@ require __DIR__ . '/../eloquent.php';
     $invoice->due_date = (new \Carbon\Carbon())->addDays(10);
     $invoice->save();
 
-    $items = [['Item 1', 1, 15] , ['Item 2', 2, 20], ['Item 3', 3, 25]];
+    $items = [['Item 1', 1, 15], ['Item 2', 2, 20], ['Item 3', 3, 25]];
 
-    foreach($items as [$description, $quantity, $unitPrice]) {
+    foreach ($items as [$description, $quantity, $unitPrice]) {
         $item = new \App\Models\InvoiceItem();
         $item->description = $description;
         $item->quantity = $quantity;
@@ -34,22 +32,19 @@ require __DIR__ . '/../eloquent.php';
     }
 });
 
+// \Illuminate\Database\Capsule\Manager::connection()->table('invoices')->where('status', InvoiceStatus::Paid)->get();
 
+// var_dump($invoices);
 
 // $invoice = new \App\Models\Invoice();
-
 
 // $invoice->amount = 45;
 // $invoice->invoice_number = '1';
 // $invoice->status = \App\Enums\InvoiceStatus::Pending;
-// // $invoice->due_date = new \DateTime('2023-01-01'); // without Carbon.
+//  $invoice->due_date = new \DateTime('2023-01-01'); // without Carbon.
 // $invoice->due_date = (new \Carbon\Carbon())->addDays(10);
 
-
-
 // $invoice->save();
-
-
 
 // $items = [['Item 1', 1, 15] , ['Item 2', 2, 20], ['Item 3', 3, 25]];
 
@@ -59,10 +54,30 @@ require __DIR__ . '/../eloquent.php';
 //     $item->quantity = $quantity;
 //     $item->unit_price = $unitPrice;
 
-//     // $item->invoice_id = $invoice->id; // Instead of doing it like this, manually, use the 'associate' method, inside the object you want to relate this item to, as seen below, with 'associate()':
+//      $item->invoice_id = $invoice->id; // Instead of doing it like this, manually, use the 'associate' method, inside the object you want to relate this item to, as seen below, with 'associate()':
 //     $item->invoice()->associate($invoice);
 
 //     $item->save();
 // }
 
+// * Update a specific invoice:
+Invoice::query()->where('id', '=', 5)->update(['status' => InvoiceStatus::Paid]);
 
+// * Select all paid invoices, then print them out:
+Invoice::query()
+    ->where('status', InvoiceStatus::Paid)
+    ->get()
+    ->each(
+        function (Invoice $invoice) {
+            echo $invoice->id . ', ' . $invoice->status->toString() . ', ' . $invoice->created_at->format('m/d/Y') . PHP_EOL;
+
+            // $invoice->items()->where('description', 'awdwdwas')->update(['description' => 'foo bar']);
+
+            // * Select the first invoice item of each invoice:
+            $item = $invoice->items()->first();
+
+            var_dump($item->id);  // This will return the ID of the first invoice item of each invoice.
+
+            // $item->delete();  // This deletes the first invoice item of each invoice.
+        }
+    );
