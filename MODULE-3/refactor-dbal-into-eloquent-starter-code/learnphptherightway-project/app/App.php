11e82166114f2a823406ac1a6561d1e7ebe18107
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-08-31 18:38:27 -0300

    finished lesson 22

---
+++ MODULE-3/refactor-dbal-into-eloquent-starter-code/learnphptherightway-project/app/App.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+use App\Exceptions\RouteNotFoundException;
+use App\Services\PaymentGatewayService;
+use App\Services\PaymentGatewayServiceInterface;
+use Dotenv\Dotenv;
+use Illuminate\Container\Container;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Symfony\Component\Mailer\MailerInterface;
+
+class App
+{
+    // private static DB $db;
+    private Config $config;
+
+    public function __construct(
+        protected Container $container,
+        protected ?Router $router = null,
+        protected array $request = [],
+    ) {}
+
+    public function boot(): static
+    {
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+        $dotenv->load();
+
+        $this->config = new Config($_ENV);
+
+        // static::$db = new DB($this->config->db ?? []); // This will be replaced by the eloquent usage, instead of this usage of DBAL, this wrapper around DBAL.
+
+        $this->initDb($this->config->db ?? []);
+
+        // ! In laravel, the 'set' method is called 'bind'
+        // $this->container->set(PaymentGatewayServiceInterface::class, PaymentGatewayService::class);
+        // $this->container->set(MailerInterface::class, fn() => new CustomMailer($this->config->mailer['dsn']));
+
+        $this->container->bind(PaymentGatewayServiceInterface::class, PaymentGatewayService::class);
+        $this->container->bind(MailerInterface::class, fn() => new CustomMailer($this->config->mailer['dsn']));
+
+        return $this;
+    }
+
+    // public static function db(): DB
+    // {
+    //     return static::$db;
+    // }
+
+    public function initDb(array $config): void  // This is the eloquent implementation, replacing  DBAL implementation
+    {
+        $capsule = new Capsule;
+
+        $capsule->addConnection($config);
+        $capsule->setEventDispatcher(new Dispatcher($this->container));
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+    }
+
+    public function run()
+    {
+        try {
+            echo $this->router->resolve($this->request['uri'], strtolower($this->request['method']));
+        } catch (RouteNotFoundException) {
+            http_response_code(404);
+
+            echo View::make('error/404');
+        }
+    }
+}
