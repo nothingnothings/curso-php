24d33b7a5f99ed24b9c28f46c52720200932da10
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-09-09 18:04:41 -0300

    installed corejs and bootstrap-icons as dependencies

--- MODULE4-EXPENNIES/13-crud-actions-for-categories/app/Controllers/CategoriesController.php
+++ MODULE4-EXPENNIES/13-crud-actions-for-categories/app/Controllers/CategoriesController.php
@@ -5,9 +5,32 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Models\Category;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use Slim\Views\Twig;
 
+class CategoriesController
+{
 
-class CategoriesController {
+    public function __construct(private readonly Twig $twig) {}
 
-    public function __construct(private readonly Twig)
+    public function index(Request $request, Response $response): Response
+    {
+
+        return $this->twig->render($response, 'categories/index.twig');
+    }
+
+    public function store(Request $request, Response $response): Response
+    {
+        // TODO
+
+        return $response->withHeader('Location', '/categories')->withStatus(302);
+    }
+
+    public function delete(Request $request, Response $response): Response
+    {
+        // TODO
+
+        return $response->withHeader('Location', '/categories')->withStatus(302);
+    }
 }
